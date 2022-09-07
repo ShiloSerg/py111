a971c91e93cd8cfb0b62ae3c01a1d7478ec7c469
Sergey Shilo <Parol24051989!>
2022-09-07 10:55:20 +0300

    changes in a3

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -9,11 +9,8 @@ def check_brackets(brackets_row: str) -> bool:
         return True
     if brackets_row[0] == ')':
         return False
-    list_of_values = ['(', ')']
     counter = 0
     for value in brackets_row:
-        if value not in list_of_values:
-            return False
         if value == '(':
             counter += 1
         if value == ')':
