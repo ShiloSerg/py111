3605052bdd513828fe3c0f523979210c80565f93
Sergey Shilo <Parol24051989!>
2022-09-07 10:36:51 +0300

    a3 done

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,20 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-
-    return False
+    if len(brackets_row) == 0:
+        return True
+    if brackets_row[0] == ')':
+        return False
+    list_of_values = ['(', ')']
+    counter = 0
+    for value in brackets_row:
+        if value not in list_of_values:
+            return False
+        if value == '(':
+            counter += 1
+        if value == ')':
+            counter -= 1
+    if counter == 0:
+        return True
+    else:
+        return False
