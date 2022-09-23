e96e0b8644fd3f8ac143984228fc47b001360f66
Sergey Shilo <Parol24051989!>
2022-09-23 17:56:05 +0300

    d0 test

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,28 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
-    print(stairway)
-    return 0
+    stairway = list(stairway)
+    list_of_min_values = []
+    while True:
+        if len(stairway) == 0:
+            print(list_of_min_values)
+            print(sum(list_of_min_values))
+            return sum(list_of_min_values)
+        elif len(stairway) == 1:
+            list_of_min_values.append(stairway[0])
+            del stairway[0]
+        elif stairway[0] < stairway[1]:
+            list_of_min_values.append(stairway[0])
+            del stairway[0]
+        elif stairway[0] > stairway[1]:
+            list_of_min_values.append(stairway[1])
+            del stairway[1]
+            del stairway[0]
+        else:
+            if stairway[0] == stairway[1]:
+                list_of_min_values.append(stairway[1])
+                del stairway[1]
+                del stairway[0]
+
+    # print(get_coast(6))
+
